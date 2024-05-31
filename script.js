e112d13051e2a728e9830094549a936ea40a1a44
Marshall Peck <marshallpeck@gmail.com>
2024-05-31 12:00:09 -0500

    Update script.js
    p5 script test

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // script.js
+
+// Log Hello, World! to the console
+console.log("Hello, World!");
+
 function changeText() {
     document.getElementById("dynamic-text").innerText = "Text has been changed!";
 }
 
+// p5.js setup function
+function setup() {
+    // Create a canvas 600x400
+    createCanvas(600, 400);
+}
+
+// p5.js draw function
+function draw() {
+    // Set background color
+    background(0);
+
+    // Draw an ellipse at the mouse position
+    fill(255);
+    ellipse(mouseX, mouseY, 50, 50);
+}
