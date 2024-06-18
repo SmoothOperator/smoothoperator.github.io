c9d0613de4053268f3c070f13f9341befc49e5b5
Marshall Peck <marshallpeck@gmail.com>
2024-06-18 14:09:19 -0500

    Update form.js

--- form.js
+++ form.js
@@ -6,10 +6,9 @@ document.getElementById('contact-form').addEventListener('submit', function(even
   const message = document.getElementById('message').value;
 
   const data = {
-    service_id: 'service_b2wr6md',
-    template_id: 'emplate_7huea39',
-    user_id: '
-email_r4vFo6Eg3Vv2lkcFqQB9i3ni',
+    service_id: 'YOUR_EMAILJS_SERVICE_ID', // Replace with your actual service ID
+    template_id: 'YOUR_EMAILJS_TEMPLATE_ID', // Replace with your actual template ID
+    user_id: 'email_r4vFo6Eg3Vv2lkcFqQB9i3ni',
     template_params: {
       'name': name,
       'email': email,
