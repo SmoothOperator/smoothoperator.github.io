46b10e63347678629feb8610fb13dbb3d9d6bf90
Marshall Peck <marshallpeck@gmail.com>
2024-09-05 18:19:42 -0500

    Update thumbnail.js

--- scripts/thumbnail.js
+++ scripts/thumbnail.js
@@ -1,4 +1,11 @@
-document.getElementById("thumbnail").addEventListener("click", function() {
-    var fullsizeImg = document.getElementById("fullsize");
-    fullsizeImg.style.display = fullsizeImg.style.display === "none" ? "block" : "none"; // Toggle visibility
+// Toggle the first image (Voxel OX Render)
+document.getElementById("thumbnail-1").addEventListener("click", function() {
+    var fullsizeImg1 = document.getElementById("fullsize-1");
+    fullsizeImg1.style.display = fullsizeImg1.style.display === "none" ? "block" : "none"; // Toggle visibility
+});
+
+// Toggle the second image (Assembled Voxel OX)
+document.getElementById("thumbnail-2").addEventListener("click", function() {
+    var fullsizeImg2 = document.getElementById("fullsize-2");
+    fullsizeImg2.style.display = fullsizeImg2.style.display === "none" ? "block" : "none"; // Toggle visibility
 });
