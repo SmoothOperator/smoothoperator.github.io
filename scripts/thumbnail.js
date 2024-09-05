1edc9f6e100abcff2022e86203094be9f63be1ae
Marshall Peck <marshallpeck@gmail.com>
2024-09-05 18:02:54 -0500

    Create thumbnail.js

---
+++ scripts/thumbnail.js
@@ -0,0 +1,4 @@
+document.getElementById("thumbnail").addEventListener("click", function() {
+    var fullsizeImg = document.getElementById("fullsize");
+    fullsizeImg.style.display = fullsizeImg.style.display === "none" ? "block" : "none"; // Toggle visibility
+});
